50b73826bc3ac9e51067bf7e6d099ccbca1c6f32
xyyx <xyyx@mail.ru>
2017-08-31 22:21:19 +0800

    Add LegacySettings proto && fix derp :(

--- packages/SystemUI/src/com/android/systemui/qs/tiles/LteTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/LteTile.java
@@ -67,7 +67,7 @@ public class LteTile extends QSTileImpl<BooleanState> {
 
     @Override
     public int getMetricsCategory() {
-        return MetricsEvent.NITROGEN_SETTINGS;
+        return MetricsEvent.LEGACY_SETTINGS;
     }
 
     @Override
